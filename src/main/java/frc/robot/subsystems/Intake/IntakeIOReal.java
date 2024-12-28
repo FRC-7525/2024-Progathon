18af1a788f50b1aa1f5b08a5f71a8991428e6d72
PotmanNob <Potmannob@outlook.com>
2024-12-27 18:44:29 -0600

    real requested changes

--- src/main/java/frc/robot/subsystems/Intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOReal.java
@@ -4,6 +4,9 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import frc.robot.GlobalConstants;
+import frc.robot.GlobalConstants.RobotMode;
 
 public class IntakeIOReal implements IntakeIO {
     private TalonFX wheelMotor;
@@ -12,14 +15,21 @@ public class IntakeIOReal implements IntakeIO {
     private PIDController wheelSpeedController;
     private double pivotPositionSetpoint;
     private double wheelSpeedSetpoint;
-
+    
     public IntakeIOReal() {
         wheelMotor = new TalonFX(IntakeConstants.Real.WHEEL_MOTOR_CANID);
         pivotMotor = new TalonFX(IntakeConstants.Real.PIVOT_MOTOR_CANID);
+        wheelMotor.setPosition(0);
+        pivotMotor.setPosition(0);
         pivotController = new PIDController(IntakeConstants.Real.PIVOT_PID_CONSTANTS.kP,
         IntakeConstants.Real.PIVOT_PID_CONSTANTS.kI, IntakeConstants.Real.PIVOT_PID_CONSTANTS.kD);
         wheelSpeedController = new PIDController(IntakeConstants.Real.WHEEL_PID_CONSTANTS.kP,
         IntakeConstants.Real.WHEEL_PID_CONSTANTS.kI, IntakeConstants.Real.WHEEL_PID_CONSTANTS.kD);
+
+        if (GlobalConstants.ROBOT_MODE == RobotMode.TESTING) {
+            SendableRegistry.setName(pivotController, "Intake", "Pivot PIDController");
+            SendableRegistry.setName(wheelSpeedController, "Intake", "Wheel Speed PIDController");
+        }
     }
 
     @Override
